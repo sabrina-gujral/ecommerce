bbc5d56786f4c2fe859cbcb02d35616a81c20c2c
Sabrina Gujral <sabrinagujral26@gmail.com>
2021-06-29 02:36:19 +0530

    Remove font family to App.css

--- src/components/menuitem/menuitem.jsx
+++ src/components/menuitem/menuitem.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { withRouter } from "react-router";
+import { Link } from "react-router-dom";
 import "./menuitem.scss";
 
-export default function MenuItem({ title, img, url, size }) {
+function MenuItem({ title, img, url, size, history, match }) {
   return (
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}/${url}`)}>
     <div className='bg-img' style={{backgroundImage: `url(${img})`}}  />
       <div className="content">
-        {/* <Link to={url}> */}
+        <Link to={url}>
           <h1 className="title">{title.toUpperCase()}</h1>
           <span className="subtitle">SHOP NOW</span>
-        {/* </Link> */}
+        </Link>
       </div>
     </div>
   );
 }
+
+export default withRouter(MenuItem)
