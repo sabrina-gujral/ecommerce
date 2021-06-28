42df45da8bcc7eb6f01c14aec766c792ea335e6b
Sabrina Gujral <sabrinagujral26@gmail.com>
2021-06-29 00:16:22 +0530

    Add homepage

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./homepage.styles.scss";
+import Directory from '../../components/directory/directory';
+
+class Homepage extends React.Component {
+    render() {
+        return (
+            <div className="homepage">
+              <Directory />
+            </div>
+          );
+    }
+}
+
+export default Homepage;
